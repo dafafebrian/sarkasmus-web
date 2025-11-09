1f58136afdec1473405591b40650fb33cd3df55a
dafafebrian <dafafebrian830@gmail.com>
2025-11-09 20:35:57 +0700

    halaman awal

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home - Sarkasmus</title>
+</head>
+<body>
+    <h1>Selamat Datang di Aplikasi Sarkasmus </h1>
+    <p>Proyek ini sedang dalam tahap pengembangan.</p>
+</body>
+</html>
