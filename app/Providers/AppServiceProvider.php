f4b0cb66bd63ce17050f06ea92a7cf524f1d914c
dafafebrian <dafafebrian830@gmail.com>
2026-01-21 20:41:53 +0700

    update perbaikan

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,21 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <--- WAJIB ADA BARIS INI DI ATAS
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // ... kode lain (seperti Paginator) biarkan saja ...
+
+        // PAKSA HTTPS (Tanpa Syarat)
+        // Tulis baris ini di paling bawah fungsi boot
+        URL::forceScheme('https'); 
     }
 }
